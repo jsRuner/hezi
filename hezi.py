37c45c6c558eaa4e7a53c0b12b98eaf11268da07
jsRuner <hi_php@163.com>
2016-02-20 16:46:02 +0800

    初始化操作

---
+++ hezi.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+
+"""
+@version: 1.0
+@author: hiphp
+@license: Apache Licence 
+@contact: hi_php@163.com
+@site: wuwenfu.cn
+@software: PyCharm Community Edition
+@file: hezi.py
+@time: 2016/2/20 16:15
+
+2016年2月20日 吴文付。
+
+获取论坛。对应关键字的帖子。发送到指定的邮箱。
+
+邮件内容是 多个符合要求的帖子链接和标题。
+
+
+为了避免发送过多。1个小时只能发送1次。
+
+
+"""
+
+
+def func():
+    pass
+
+
+class Main():
+    def __init__(self):
+        pass
+
+
+if __name__ == '__main__':
+    pass
